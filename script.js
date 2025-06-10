e2924b5e01afa19ea2dfdd7b5bf5a56c6f42b8a9
sekiro <sekirodeathe@gmail.com>
2025-06-10 22:47:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,23 @@ const form = document.getElementById('form');
 const supportForm = document.getElementById('support-form');
 const successMessage = document.getElementById('success-message');
 const sendAgainBtn = document.getElementById('send-again');
+const successText = document.getElementById('success-text');
 
 form.addEventListener('submit', function (e) {
   e.preventDefault();
 
+  const firstName = document.getElementById('firstName').value.trim();
+  const email = document.getElementById('email').value.trim();
   const consent = document.getElementById('consent');
+
   if (!consent.checked) {
     alert('Вы должны согласиться на обработку персональных данных.');
     return;
   }
+
+  const userName = firstName || email;
+
+  successText.innerText = `Спасибо, ${userName}!\nСообщение отправлено!`;
 
   supportForm.classList.add('hidden');
   successMessage.classList.remove('hidden');
